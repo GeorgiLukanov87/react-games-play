9aaa6f18110def0cab3b04d846956c652361459a
Georgi St. Lukanov <goshiii87@gmail.com>
2023-09-04 13:23:00 +0200

    install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 import Header from './components/header/Header'
 import Home from './components/home/Home';
-
+import { Routes, Route } from 'react-router-dom'
 function App() {
     return (
         <div id="box">
             <Header />
-            
-            <main id="main-content">
-                <Home />
-            </main>
 
+            <main id="main-content">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+
+            </main>
 
         </div>
 
