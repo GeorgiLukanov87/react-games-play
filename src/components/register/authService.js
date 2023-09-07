8456b2ae5930038a69202eb411121da9ef986eeb
Georgi St. Lukanov <goshiii87@gmail.com>
2023-09-07 22:50:48 +0200

    .

--- src/components/register/authService.js
+++ src/components/register/authService.js
@@ -2,6 +2,5 @@ import * as request from "./requester";
 
 const baseUrl = 'http://localhost:3030';
 
-export const login = (email, password) => {
-    request.post(`${baseUrl}/data/games/`)
-}
+export const login = (email, password) =>
+    request.post(`${baseUrl}/users/login`, { email, password })
--- src/services/gameService.js
+++ src/services/gameService.js
@@ -1,6 +1,6 @@
 import * as request from "../components/register/requester";
 
-const baseUrl = 'http://localhost:3030/data/games/';
+const baseUrl = 'http://localhost:3030/data/games';
 
 export const getAll = () => request.get(baseUrl);
     
